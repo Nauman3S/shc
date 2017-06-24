7884265358e7a5444b5394f8e4fd9437bd0cb12e
Nauman Shakir <naumanshakir3s@gmail.com>
2017-06-25 00:34:28 +0500

    Create fla.sh

---
+++ fla.sh
@@ -0,0 +1,7 @@
+sudo apt-get install libxext-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libimlib2-dev
+sudo apt-get install libglew-dev
+sudo apt-get install libxrender-dev
+sudo apt-get install libxrandr-dev
+sudo apt-get install libglm-dev
